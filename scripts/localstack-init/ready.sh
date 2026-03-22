8060adc7cad4cdbecf900ab852c005f7188d1e8c
KANG_the_Conqueror <139243766+KANG-HYUNIL@users.noreply.github.com>
2026-03-22 23:26:04 +0900

    FIX : e2e test LocalStack 헬스체크 제한 시간 증가

--- scripts/localstack-init/ready.sh
+++ scripts/localstack-init/ready.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+set -x
 # [LF-FIX-20260322] Ensuring Linux-compatible line endings for LocalStack
-echo "----------- Initializing LocalStack Resources -----------"
+echo "----------- Initializing LocalStack Resources -----------" | tee /tmp/localstack_init.log
 
 # 1. S3 Buckets
 awslocal s3 mb s3://taco5-graphnode-graphdata-s3
@@ -10,6 +11,6 @@ awslocal s3 mb s3://taco5-graphnode-filedata-chat-and-note-s3
 awslocal sqs create-queue --queue-name taco-graphnode-request-graph-sqs
 awslocal sqs create-queue --queue-name taco-graphnode-response-graph-sqs
 
-echo "----------- LocalStack Resources Created -----------"
-awslocal s3 ls
-awslocal sqs list-queues
+echo "----------- LocalStack Resources Created -----------" | tee -a /tmp/localstack_init.log
+awslocal s3 ls | tee -a /tmp/localstack_init.log
+awslocal sqs list-queues | tee -a /tmp/localstack_init.log
